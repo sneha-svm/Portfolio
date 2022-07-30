e5612ffa11081ba5090b5b5c793ffbde2fb28288
Sneha V <83590518+sneha-svm@users.noreply.github.com>
2022-07-30 14:26:14 +0530

    Add files via upload

---
+++ Task 2/index.js
@@ -0,0 +1,13 @@
+function scr(){
+    window.scrollTo(0,670);
+}
+function scr1(){
+    window.scrollTo(0,0);
+}
+function scr2(){
+    window.scrollTo(0,1340);
+}
+function scr3(){
+    window.scrollTo(0,2010);
+    ('rick',scr1);
+}
